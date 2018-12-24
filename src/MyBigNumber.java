8828bf5c313b5167c549cf00e6963a5f30c39302
zero0z0 <43369568+zero0z0@users.noreply.github.com>
2018-12-24 12:16:10 +0700

    Update MyBigNumber.java

--- src/MyBigNumber.java
+++ src/MyBigNumber.java
@@ -1,4 +1,7 @@
-﻿
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 
 
 /**
@@ -32,6 +35,7 @@ public class MyBigNumber {
         int num;
         int ghi;
         String s = "";
+        String tong = "";
         
         
         int num1 = 0; // dùng để ghép các chữ số lại thành 1 số
@@ -51,55 +55,49 @@ public class MyBigNumber {
         final Matcher isError1 = pattern.matcher(s1);// biến để lưu dữ kết quả xét chuỗi s1 
         final Matcher isError2 = pattern.matcher(s2);;// biến để lưu dữ kết quả xét chuỗi s2
 
-        // Nếu số nhập vào là âm thì báo lỗi
+        // Bắt lỗi dữ liệu nhập vào nếu có
+        
+        
+
+        // Nếu số nhập vào là âm thì không tính và báo lỗi
         
         
         
         if (s1.charAt(0) == '-') {
             this.ireceiver.send("Không được nhập số âm : " + s1);
             
+            //throw new NumberFormatException("Bạn vui lòng không nhập số âm : " + s1);
         }
         
 
         if (s2.charAt(0) == '-') {
             ireceiver.send("Không được nhập số âm : " + s2);
             
+            //throw new NumberFormatException("Bạn vui lòng không nhập số âm : " + s2);
         }
         
         
 
-        // Nếu nhập vào kí tự đặc biệt thì báo lỗi
+        // Nếu nhập vào kí tự đặc biệt thì không tính và báo lỗi
         
         if (isError1.find()) {
             vtri = isError1.start() + 1;
             this.ireceiver.send("Vị trí " + vtri + " trong chuỗi " + num1 + " không phải số");
             
+            //throw new NumberFormatException(vtri + "");
         }
 
         if (isError2.find()) {
             vtri = isError2.start() + 1;
             this.ireceiver.send("Vị trí " + vtri + " trong chuỗi " + num2 + " không phải số");
             
-            
+            //throw new NumberFormatException(vtri + "");
 
         }
         
         int max = s1.length();
         int i = 0;
-        
-        for (i = s1.length() - 1; i >= 0; i--) {
-            num1 += (s1.charAt(i) - '0') * tmp1; // '0' vì 0 là chữ số đầu tiên từ 0 0 đến 10
-            num3[i] = (s1.charAt(i) - '0');
-            tmp1 *= 10;  // sau khi từ hàng đơn vị sẽ chuyển lên hàng chục và đến hàng trăm ,....
-        }
-        
-        for (i = s2.length() - 1; i >= 0; i--) {
-            num2 += (s2.charAt(i) - '0') * tmp2; 
-            num4[i] = (s2.charAt(i) - '0');
-            tmp2 *= 10;
-        }
-        
-        
+    
         if (max < s2.length()) {
             
             max = s2.length();
@@ -109,31 +107,35 @@ public class MyBigNumber {
             max = s1.length();
             
         }
-        
-        
-        
-        int j;
-        for (i = max - 3,j = 1; i >= 0;j++,i--) {
-            num = num3[i] + num4[i];
+    
+        for (i = 1 ; i <= max ; i++) {
+            c1 = ((s1.length() - i) >= 0) ? s1.charAt(s1.length() - i) : '0';
+            c2 = ((s2.length() - i) >= 0) ? s2.charAt(s2.length() - i) : '0';
+            
+            num = (c1 - '0') + (c2 - '0');
+            tong = Integer.toString(num % 10) + tong;
+    
+            //s+=tong;
             temp1 =  num % 10;
             if (temp2 == 0) {
-                s += "buoc" + j + ":" + "lay" + num3[i] + " cong " + num4[i] + " duoc " 
-                    + (num3[i] + num4[i]) + " ghi " + temp1 + " nho " + num / 10 ;
+                s += "buoc" + i + ":" + "lay" + c1 + " cong " + c2 + " duoc " 
+                    + (num) + " ghi " + temp1 + " nho " + num / 10 + "\n";
             } else {
-                s += "buoc" + j + ":" + " lay " + num3[i] + " cong " + num4[i] + " duoc "
-                    + (num3[i] + num4[i]) + " cong con " + temp2 + " bang " + (num + temp2)
-                    + "ghi con" + (num + temp2) % 10 + "nho" + temp2;
+                s += "buoc" + i + ":" + " lay " + c1 + " cong " + c2 + " duoc "
+                    + (num) + " cong con " + temp2 + " bang " + (num + temp2)
+                    + " ghi con " + (num + temp2) % 10 + " nho " + temp2 + "\n";
             }
             temp2 = num / 10;
     
         }
         if (temp2 > 0) {
-            s += "\n" + "Buoc " + j + ": " + "lay " + 0 + ", cong " + 0 + ", nho " 
+            s += "Buoc " + i + ": " + "lay " + 0 + ", cong " + 0 + ", nho " 
                 + 1 + ", bang " + 1 + ", viet " + 1 + "\n";
         }
-    
         
-        s += String.format("%d",(num1 + num2));
+        s += "\n" + "ket qua: " + tong;
+        
+        //s += String.format("%d",(num1 + num2));
         
         
         return s;
